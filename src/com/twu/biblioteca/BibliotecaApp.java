d10af604edda1ce2c615e14421d3a2189ecea27f
hhy <1014596312@qq.com>
2018-07-11 11:58:44 +0800

    Welcome Message Done

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,4 +5,8 @@ public class BibliotecaApp {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
+
+    public static void welcome() {
+        System.out.println("Welcome to Biblioteca!");
+    }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,33 @@
 package com.twu.biblioteca;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
+    private final PrintStream originalOut = System.out;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testWelcomeMessage() {
+        BibliotecaApp.welcome();
+        assertEquals("Welcome to Biblioteca!\r\n", outContent.toString());
     }
 }
